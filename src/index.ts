276a07a40f406416dd44c914e27c2acfd28c8c75
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-03-10 07:28:01 -0300

    chore: added cors and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-import express from 'express'
+require('dotenv').config();
+import express from 'express';
 import { createConnection } from 'typeorm';
 import { routes } from './routes';
+import cors from 'cors'
 
 createConnection().then(() => {
 	const app = express();
 
 	app.use(express.json());
+	app.use(cors({
+		origin: ['http://localhost:3000'],
+		credentials: true
+	}))
 
 	routes(app)
 
