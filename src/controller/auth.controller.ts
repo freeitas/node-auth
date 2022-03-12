6b881774a7beedbb2f7246ae6fc5ef42a8353f97
Guilherme Freitas <hfdsguilherme@gmail.com>
2022-03-12 09:34:27 -0300

    feat: refresh token

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -91,3 +91,34 @@ export const AuthenticatedUser = async (req: Request, res: Response) => {
 	}
 
 }
+
+export const Refresh = async (req: Request, res: Response) => {
+		try {		
+			const cookie = req.cookies['refresh_token']
+
+			const payload: any = verify(cookie, process.env.REFRESH_SECRET || '')
+
+			if(!payload) {
+				return res.status(401).send({
+					message: 'unauthenticated'
+				})
+			}
+
+				const accessToken = sign({
+					id: payload.id
+				}, process.env.ACCESS_SECRET || '', {expiresIn: '30s'})
+
+				res.cookie('access_token', accessToken, {
+					httpOnly: true,
+					maxAge: 24*60*60*100 // 1 day
+				})
+
+				res.send({
+					message: 'success'
+				})
+		} catch(e) {
+			return res.status(401).send({
+					message: 'unauthenticated'
+			})
+		}
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { Register, Login, AuthenticatedUser } from "./controller/auth.controller"
+import { Register, Login, AuthenticatedUser, Refresh } from "./controller/auth.controller"
 
 export const routes = (router: Router) => {
 	router.post('/api/register', Register)
 	router.post('/api/login', Login)
-	router.get('/api/user', AuthenticatedUser )
+	router.get('/api/user', AuthenticatedUser)
+	router.post('/api/refresh', Refresh)
 }
